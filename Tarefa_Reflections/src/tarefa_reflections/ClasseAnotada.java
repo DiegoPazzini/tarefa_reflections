43d889356bbe76c1a09d2f39eb7c787097388067
Diego Pazzini <diegopazzini2009@hotmail.com>
2023-10-20 04:57:23 -0300

    Update ClasseAnotada.java

--- Tarefa_Reflections/src/tarefa_reflections/ClasseAnotada.java
+++ Tarefa_Reflections/src/tarefa_reflections/ClasseAnotada.java
@@ -3,3 +3,4 @@ package tarefa_reflections;
 @Tabela("nome_da_tabela")
 public class ClasseAnotada {
 }
+//Neste trecho de código, a anotação @Tabela é utilizada para marcar a classe ClasseAnotada com metadados adicionais. As anotações em Java fornecem metadados sobre elementos do programa, como classes, métodos, variáveis e outros.
